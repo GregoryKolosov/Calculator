eacf16a8162e39495e6a9ad75f904ff39c7066f3
GregoryKolosov <90kolosov@gmail.com>
2018-07-19 17:51:17 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Gregory Kolosov on 19/07/2018.
+//  Copyright © 2018 Gregory Kolosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
